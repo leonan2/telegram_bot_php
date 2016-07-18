12b6d639cabe08d4b03630b5901811aeefe333f2
Anderson Botelho Cerqueira <andersonbc.93@gmail.com>
2016-07-18 17:34:02 -0300

    Update botestudo.php

--- botestudo.php
+++ botestudo.php
@@ -7,4 +7,8 @@ $update = file_get_contents($website."/getupdates");
 
 $updateArray = json_decode($update, TRUE);
 
-print_r($updateArray);
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+
+//print_r($updateArray);
