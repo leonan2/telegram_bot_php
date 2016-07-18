9a3c1309a7e22aa0abd3dfba6994073d46aed01b
Anderson Botelho Cerqueira <andersonbc.93@gmail.com>
2016-07-18 17:09:12 -0300

    Update botestudo.php

--- botestudo.php
+++ botestudo.php
@@ -1,6 +1,6 @@
 <?php
 
-$botTokeb = "263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc";
+$botToken = "263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $update = file_get_contents($website."/getupdates");
