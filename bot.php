75e5ec888a85a6e1002921e7d961ab3c0ee54743
Anderson Botelho Cerqueira <andersonbc.93@gmail.com>
2016-07-18 23:27:08 -0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@ use Telegram\Bot\Api;
 
 $telegram = new Api('263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc');
 
-
+/*
 $response = $telegram->getMe();
 
 $botId = $response->getId();
@@ -13,3 +13,15 @@ $firstName = $response->getFirstName();
 $username = $response->getUsername();
 
 echo $response;
+*/
+
+$updates = $telegram->getWebhookUpdates();
+//$response = $telegram->getUpdates();
+
+  $updateArray = json_decode($updates, TRUE);
+  $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+  $response = $telegram->sendMessage([
+    'chat_id' => '149525498',
+    'text' => 'Hello World'
+  ]);
