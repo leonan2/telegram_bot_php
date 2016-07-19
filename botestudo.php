e616498ce2db54e786d7d3e470c52a878c43e41f
Anderson Botelho Cerqueira <andersonbc.93@gmail.com>
2016-07-18 23:00:06 -0300

    Update botestudo.php

--- botestudo.php
+++ botestudo.php
@@ -1,9 +1,9 @@
 <?php
 $botToken = "263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc";
 $website = "https://api.telegram.org/bot".$botToken;
-//$update = file_get_contents($website."/getupdates");
+$update = file_get_contents($website."/getupdates");
 //webhook
-$update = file_get_contents("php://input");
+//$update = file_get_contents("php://input");
 $updateArray = json_decode($update, TRUE);
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
 file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
