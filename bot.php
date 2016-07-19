f546010925d864a84e9d258e76874b4451295c32
Anderson Botelho Cerqueira <andersonbc.93@gmail.com>
2016-07-18 21:55:04 -0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,12 @@
 <?php
 require 'vendor/autoload.php';
+
+use Telegram\Bot\Api;
+
+$telegram = new Api('BOT TOKEN');
+
+$response = $telegram->getMe();
+
+$botId = $response->getId();
+$firstName = $response->getFirstName();
+$username = $response->getUsername();
