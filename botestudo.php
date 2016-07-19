35afa8a0cc6966134791560c69f43878e78589e9
Anderson Botelho Cerqueira <andersonbc.93@gmail.com>
2016-07-18 22:58:30 -0300

    Update botestudo.php

--- botestudo.php
+++ botestudo.php
@@ -1,29 +1,11 @@
 <?php
-require 'vendor/autoload.php';
+$botToken = "263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc";
+$website = "https://api.telegram.org/bot".$botToken;
+//$update = file_get_contents($website."/getupdates");
+//webhook
+$update = file_get_contents("php://input");
+$updateArray = json_decode($update, TRUE);
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+print_r($updateArray);
 
-use Telegram\Bot\Api;
-
-$telegram = new Api('263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc');
-
-/*
-$response = $telegram->getMe();
-
-$botId = $response->getId();
-$firstName = $response->getFirstName();
-$username = $response->getUsername();
-
-echo $response;
-*/
-
-//$updates = $telegram->getWebhookUpdates();
-$response = $telegram->getUpdates();
-
-  $updateArray = json_decode($response, TRUE);
-  $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
-
-  $response = $telegram->sendMessage([
-    'chat_id' => $chatId,
-    'text' => 'Hello World'
-  ]);
-
-echo
